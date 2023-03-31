037c1c12ddc5eb043d3b66175a0ebae5db1eaf3d
IllyaC <129131802+IllyaC@users.noreply.github.com>
2023-03-30 19:28:37 -0700

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,28 +1,27 @@
 def insert(intervals, newInterval):
-        n = len(intervals)
-        if n == 0:
+         if not intervals:
             return [newInterval]
 
-        iterator_1, final = 0, []
+        index, final = 0, []
         current_start, current_end = newInterval
-        while iterator_1 < n:
+        while index < n:
             original_start, original_end = intervals[iterator_1]
             
             #bigger
             if original_end < current_start:
                 final.append([original_start, original_end])
-                iterator_1 += 1
+                index += 1
                 continue
             #smaller
             if current_end < original_start:
                 final.append([current_start, current_end])
                 current_start, current_end = original_start, original_end
-                iterator_1 += 1
+                index += 1
                 continue
             
             #overlap
             current_start, current_end = min(original_start, current_start), max(original_end, current_end)
-            iterator_1 += 1
+            index += 1
 
         final.append([current_start, current_end])
         return final
