8d59fbe0d4dd91fde9fe4b2011ece656c151b8d1
IllyaC <illya.cheb@gmail.com>
2023-04-01 13:40:17 -0700

    23/4/1 improved it alot using chatgpt assistance. not sure why I did not think of just making the reversed integer in the while loop but that was the main change.

--- reverse_integer.py
+++ reverse_integer.py
@@ -6,109 +6,25 @@ If reversing x causes the value to go outside the signed 32-bit integer range
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 """
 def reverse(integer):
+    int_min = -2 ** 31
+    int_max = 2 **31 -1
+    isNegative = False
+    
+    if integer < 0:
+        isNegative = True
+        integer = -integer
     reversed_integer = 0
-    isNegative = integer < 0
-    original_integer = abs(integer)
-    integer  = abs(integer)
-    digit_counter = -1
     
-    if(integer == -2147483648):
-        return reversed_integer
-    
-    
+    while integer != 0:
+        digit = integer%10
+        integer //= 10
         
-    while(integer != 0):
-        digit_counter +=1 
-        integer = integer // 10
-    
-    
-    integer = original_integer
-    hitMax = digit_counter == 9
-    prevHitMax = False
-
-  
-    for digit in range(digit_counter+1):
-        
-        temp = integer%10
-        if hitMax:
-            if digit_counter == 9:
-                        if temp == 2:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp > 2:
-                            return 0
-            if digit_counter == 8  and prevHitMax:
-                        if temp ==1:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp >1:
-                            return 0
-            if digit_counter == 7 and temp >4 and prevHitMax:
-                        if temp == 4:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        return 0
-            if digit_counter == 6 and prevHitMax:
-                        if temp == 7:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp > 7:
-                            return 0
-            if digit_counter == 5  and prevHitMax:
-                        if temp == 4:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp>4:
-                            return 0
-            if digit_counter == 4 and prevHitMax:
-                        if temp == 8:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp > 8:
-                            return 0
-            if digit_counter == 3  and prevHitMax:
-                        if temp == 3:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp > 3:
-                            return 0
-            if digit_counter == 2  and prevHitMax:
-                        if temp == 6:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp > 6:
-                            return 0
-            if digit_counter == 1 and prevHitMax:
-                        if temp == 4:
-                            prevHitMax = True
-                        else: 
-                            prevHitMax = False
-                        if temp > 4:
-                            return 0
-            if digit_counter == 0 and temp >7 and prevHitMax:
-                        return 0
-        
-            
-        reversed_integer += temp * (10 **digit_counter)
-        digit_counter -= 1
-        integer = integer // 10
-    
+        if reversed_integer > (int_max -digit) // 10:
+            return 0
+        reversed = reversed_integer * 10 + digit
     if(isNegative):
-        reversed_integer = reversed_integer * -1
-    
-   
+        reversed_integer = -reversed_integer
     return reversed_integer
-    
-        
-        
 integer  = 1563847412
 print(reverse(integer))
 2147483651
