4c651340a34fc7474f94417f343f72abb1eb1abc
IllyaC <illya.cheb@gmail.com>
2023-04-05 22:42:34 -0700

    23/4/5 simple traversal forwards then backwards

---
+++ summing_array.py
@@ -0,0 +1,30 @@
+"""
+Given an integer array nums, return an array 
+answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is 
+guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in 
+O(n) time and without using the division operation.
+"""
+
+def productExceptSelf(nums):
+    forwards = 1
+    backwards = 1   
+    multiplications = [1] * len(nums)
+    for i in  range(len(nums)):
+        multiplications[i] = forwards
+        forwards *= nums[i]
+    for j in  range(len(nums)-1, -1, -1):
+        multiplications[j] *= backwards
+        backwards *= nums[j]
+    
+    return multiplications
+
+nums = [-1,1,0,-3,3]
+print(productExceptSelf(nums))
+     
+        
+        
+        
